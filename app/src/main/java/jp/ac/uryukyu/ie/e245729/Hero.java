cfe83affe63c70352021e6f54d947df92a40b1c4
Nakaishi Kousuke <e245729@ie.u-ryukyu.ac.jp>
2024-11-12 20:40:24 +0900

    3rd

--- app/src/main/java/jp/ac/uryukyu/ie/e245729/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245729/Hero.java
@@ -11,10 +11,10 @@ package jp.ac.uryukyu.ie.e245729;
  * Created by tnal on 2016/11/13.
  */
 public class Hero {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -36,9 +36,11 @@ public class Hero {
      * @param e 攻撃対象
      */
     public void attack(Enemy e){
+       
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
         e.wounded(damage);
+        
     }
 
     /**
@@ -52,5 +54,18 @@ public class Hero {
             dead = true;
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
         }
+    }
+    public String getName(){
+        return name;
+
+    }
+    public int getHitPoint(){
+        return hitPoint;
+    }
+    public int getAttack(){
+        return attack;
+    }
+    public boolean getDead(){
+        return dead;
     }
 }
